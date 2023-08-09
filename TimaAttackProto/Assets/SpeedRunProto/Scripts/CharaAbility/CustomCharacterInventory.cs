050582521e0faea90a708132a0478851e93f5298
Tarosism <yar0606@naver.com>
2023-08-10 08:10:05 +0900

    Update CustomCharacterInventory.cs

--- TimaAttackProto/Assets/SpeedRunProto/Scripts/CharaAbility/CustomCharacterInventory.cs
+++ TimaAttackProto/Assets/SpeedRunProto/Scripts/CharaAbility/CustomCharacterInventory.cs
@@ -35,6 +35,23 @@ namespace MoreMountains.CorgiEngine
             }
         }
 
+        protected virtual void EquipSecondaryWeapon(string weaponID)
+        {
+            // Logic to equip a weapon into the secondary weapon inventory
+            for (int i = 0; i < MainInventory.Content.Length; i++)
+            {
+                if (InventoryItem.IsNull(MainInventory.Content[i]))
+                {
+                    continue;
+                }
+                if (MainInventory.Content[i].ItemID == weaponID)
+                {
+                    MMInventoryEvent.Trigger(MMInventoryEventType.EquipRequest, null, SecondaryWeaponInventoryName, MainInventory.Content[i], 0, i, PlayerID);
+                    break;
+                }
+            }
+        }
+
         // 여기에 secondary weapon inventory와 관련된 추가 메서드 및 로직을 추가할 수 있습니다.
     }
 }
